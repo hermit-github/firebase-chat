3ad8339e80a6be817c2ebd280bf76eede5974133
H3RMIT <shubhamhalder0@gmail.com>
2023-06-18 17:21:13 +0530

    Navbar wireframed

---
+++ src/components/Sidebar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { LogoutOutlined } from '@ant-design/icons'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <span className='logo'>H3RMIT Chat</span>
+        <div className='user'>
+            <img alt='user img' src='https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600'/>
+            <span>John</span>
+            <LogoutOutlined style={{cursor:'pointer'}} />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
