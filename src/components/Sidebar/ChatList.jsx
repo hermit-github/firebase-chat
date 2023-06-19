808ba0b57e0d2718dfbcc8fb8d010799eeeb9a11
H3RMIT <shubhamhalder0@gmail.com>
2023-06-19 23:38:27 +0530

    Sidebar Wireframed

--- src/components/Sidebar/ChatList.jsx
+++ src/components/Sidebar/ChatList.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const ChatList = () => {
   return (
-    <div>ChatList</div>
-  )
-}
+    <div className="chats">
+      <div className="user-chat">
+        <img
+          alt="user img"
+          src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        />
+        <div className="user-chat-info">
+          <span>Jacob</span>
+          <p>Hello There!</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          alt="user img"
+          src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        />
+        <div className="user-chat-info">
+          <span>Jacob</span>
+          <p>Hello There!</p>
+        </div>
+      </div>
+      <div className="user-chat">
+        <img
+          alt="user img"
+          src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        />
+        <div className="user-chat-info">
+          <span>Jacob</span>
+          <p>Hello There!</p>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
 
-export default ChatList
+export default ChatList;
--- src/components/Sidebar/Search.jsx
+++ src/components/Sidebar/Search.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className='search-form'>
+        <input type='text' placeholder='find a user'/>
+      </div>
+
+      <div className='user-chat'>
+        <img alt='user img' src='https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600'/>
+        <div className='user-chat-info'>
+          <span>Jacob</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
